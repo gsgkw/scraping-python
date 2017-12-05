0952c40263956742d67061cce506e7fb42d53ebd
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-12-05 16:22:04 +0000

    scraper returns one headline

--- imdb-news-raw.py
+++ imdb-news-raw.py
@@ -1,3 +1,5 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 html = urlopen("http://www.imdb.com/news/top")
-print(html.read())
+imdbNews = BeautifulSoup(html.read(), "html.parser")
+print(imdbNews.h2)
