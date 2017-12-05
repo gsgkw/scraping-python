7ad127acc68395355be8e6b24ea93097497f886d
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-12-05 16:26:33 +0000

    returns headline text only

--- imdb-news-raw.py
+++ imdb-news-raw.py
@@ -3,4 +3,4 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.imdb.com/news/top")
 imdbNews = BeautifulSoup(html, "html.parser")
 for headline in imdbNews.findAll("h2"):
-    print(headline)
+    print(headline.get_text())
